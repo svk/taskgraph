8db0f5706089093f14777efc9547966ab02c97cb
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-03-26 17:51:05 +0100

    trying for allegro compatibility

--- run-program.lisp
+++ run-program.lisp
@@ -1,5 +1,29 @@
 (in-package #:cl-gpgi)
 
+#+allegro
+(defun run-program (command-line
+		    &key
+		    (search t)
+		    (input-from-string nil)
+		    (input-from-stream nil)
+		    (output-to-string nil)
+		    (output-to-stream nil))
+  ;; Mostly untested!
+  (when (not search)
+    (warn (format nil "cannot avoid searching for ~a in allegro" (car command-line))))
+  (let* ((command-line-array (make-array (list (length command-line))
+					 :initial-contents command-line))
+	 (results (excl.osi:command-output command-line-array
+					   :whole t
+					   :input (if input-from-stream
+						      #'(lambda (ws)
+							  (write-line (read-line input-from-stream)
+								      ws))
+						      input-from-string))))
+    (when output-to-string
+      (return-from run-program results))
+    (when output-to-stream
+      (princ results output-to-stream))))
 
 #+sbcl
 (defun run-program (command-line
